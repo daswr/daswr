71b09e542ead944f163d13e40a9167b7c2902a23
Riccardo Balbo <riccardo0blb@gmail.com>
2021-07-27 22:47:27 +0200

    Update DASWR.sh

--- DASWR.sh
+++ DASWR.sh
@@ -1,10 +1,10 @@
 python3 -m venv daswr-env
 source daswr-env/bin/activate
+pip3 install wheel
 pip3 install pulsectl
 pip3 install psutil  
-pip3 install pysciter
 pip3 install bleach
 pip3 install python-libxdo 
 pip3 install python-xlib
-pip3 install wheel
+pip3 install pysciter
 PATH="$PATH:$PWD/sciter/bin.lnx/x64" python3 GUI.py 
